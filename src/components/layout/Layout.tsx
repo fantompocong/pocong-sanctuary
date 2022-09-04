4e54da4d17f6cbd4ab7352e1db2a4574e642acd7
FantomPocong <94260543+fantompocong@users.noreply.github.com>
2022-09-04 18:51:29 +0700

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,9 +12,6 @@ function Layout({ children }: LayoutProps) {
       <div className="container 2xl:max-w-7xl px-4 mx-auto flex-1">
         {children}
       </div>
-      <div className="py-8" />
-      <Subscribe />
-    </div>
   );
 }
 
