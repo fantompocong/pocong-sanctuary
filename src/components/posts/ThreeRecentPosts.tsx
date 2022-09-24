bfd221604198a9a29ec353780837ab4837952525
FantomPocong <94260543+fantompocong@users.noreply.github.com>
2022-09-24 18:55:10 +0700

    Update ThreeRecentPosts.tsx

--- src/components/posts/ThreeRecentPosts.tsx
+++ src/components/posts/ThreeRecentPosts.tsx
@@ -14,7 +14,7 @@ const ThreeRecentPosts = ({ posts }: { posts: Post[] }) => {
       id="recent-posts"
       className="scroll-my-16"
     >
-        <h2 className="text-3xl font-bold hover:cursor-pointer hover:text-greenAccent transition-colors w-fit">
+        <h2 className="text-3xl font-bold hover:cursor-pointer transition-colors w-fit">
           Recent Posts{' '}
         </h2>
       <div className="mt-4 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
