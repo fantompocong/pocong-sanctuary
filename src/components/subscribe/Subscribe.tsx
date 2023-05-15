0887aa381b044e9c22d4603c0595ba48dcc4f268
FantomPocong <94260543+fantompocong@users.noreply.github.com>
2023-05-15 16:55:33 +0700

    Update Subscribe.tsx

--- src/components/subscribe/Subscribe.tsx
+++ src/components/subscribe/Subscribe.tsx
@@ -10,7 +10,7 @@ const Subscribe = () => {
       <SocialMedia />
       <div>
         <p className="text-center text-xs mt-8 opacity-60">
-          &copy; 2023 pocong.xyz - Built with Next.js, Tailwind, Contentlayer,
+          &copy; 2022 pocong.xyz - Built with Next.js, Tailwind, Contentlayer,
           Vercel | Based on bughowi.com.
         </p>
       </div>
